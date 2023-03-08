7ed725643b372142750f0d47ecc4420daa81aff4
Saba <sabooli22@gmail.com>
2023-03-09 01:45:46 +0330

    edited warning

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
-import App from "./App";
 import Main from "./Main";
 import Footnote from "./Footnote";
 import reportWebVitals from "./reportWebVitals";
