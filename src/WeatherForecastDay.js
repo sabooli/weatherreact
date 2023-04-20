e1390d5e73f0cc0b46c9169cf084b2eddb3cde7e
Saba <sabooli22@gmail.com>
2023-04-20 16:25:45 +0430

    edited extra link

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,6 +1,5 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
-import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherForecastDay(props) {
   function day() {
